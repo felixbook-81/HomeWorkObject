5c5e9419bd1d7faf065bb2d4a705fd12711f6a5b
felixbook-81 <119006656+felixbook-81@users.noreply.github.com>
2023-02-04 16:19:29 +0300

    Update Main.java
    тут тоже исправил

--- src/lesson1/Main.java
+++ src/lesson1/Main.java
@@ -6,8 +6,8 @@ public class Main {
         Author author2 = new Author("Александр", "Пушкин");
         Book book1 = new Book("Война и Мир", 1867);
         Book book2 = new Book("Онегин", 1833);
-        System.out.println("Название книги = " + book1.getauthor());
-        System.out.println("Название книги = " + book2.getauthor());
+        System.out.println("Название книги = " + book1.getbookTitle());
+        System.out.println("Название книги = " + book2.getbookTitle());
         System.out.println("Автор = " + author1.getName()+ " " + author1.getSurname());
         System.out.println("Автор = " + author2.getName()+ " " + author2.getSurname());
         System.out.println("Год Публикации = " + book1.getPublication());
@@ -16,5 +16,6 @@ public class Main {
         book1.setPublication(1938);
         // Вызываем сеттер
         System.out.println("Год Публикации =  " + book1.getPublication());
+        
     }
 }
